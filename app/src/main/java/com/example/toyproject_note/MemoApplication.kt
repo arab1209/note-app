03db9bd6709801ce6de1b4b00b55f29ab19edfbd
arab1209 <8345784@naver.com>
2025-11-12 23:30:59 +0900

    feat: Hilt 의존성 주입 설정

---
+++ app/src/main/java/com/example/toyproject_note/MemoApplication.kt
@@ -0,0 +1,8 @@
+package com.example.toyproject_note
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MemoApplication : Application() {
+}
--- app/src/main/java/com/example/toyproject_note/presentation/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/toyproject_note/presentation/main/viewmodel/MainViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.toyproject_note.presentation.main.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(): ViewModel() {
 }
