486cf3003a6998c0e0bcf73694ed0d480e48fe66
arab1209 <8345784@naver.com>
2025-11-13 13:12:13 +0900

    feat: 문자열 데이터 추가

--- app/src/main/java/com/example/toyproject_note/ui/theme/String.kt
+++ app/src/main/java/com/example/toyproject_note/ui/theme/String.kt
@@ -3,3 +3,5 @@ package com.example.toyproject_note.ui.theme
 const val MAIN_TITLE = "목록"
 const val ADD_MEMO ="메모 추가"
 const val MEMO_ID = "memoId"
+const val NOT_MEMO = "메모가 없습니다"
+const val NOT_MEMO_ADD = "새로운 메모를 추가 해 보세요"
